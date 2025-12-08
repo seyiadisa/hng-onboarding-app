12e66dfce3313d792abf213a8c08bb56b9d57550
mujeeb <shola.mujeeb@gmail.com>
2025-12-09 00:54:12 +0100

    Save changes before switching branch

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-const config = {
-  plugins: {
-    "@tailwindcss/postcss": {},
-  },
-};
-
-export default config;
-=======
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
@@ -15,4 +6,3 @@ const config = {
 }
 
 export default config
->>>>>>> a6a4143ecfa9a5789c3bae79978d91782bb1d1f9
